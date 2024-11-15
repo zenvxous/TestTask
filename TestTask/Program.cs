e6431ee13a468b2df5b3c5ef4414251aabb2e7c7
zenvxous <zenvxous@gmail.com>
2024-11-15 19:28:40 +0300

    all done

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -7,11 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-    });
+builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
